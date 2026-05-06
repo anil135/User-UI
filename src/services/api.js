f70d7a7dc0341ce4046d8df0415bbe0234274aa7
Anil Lingolu <71059714+anil135@users.noreply.github.com>
2026-05-06 22:30:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://YOUR_API_GATEWAY_URL"
+  baseURL: "https://YOUR_API_GATEWAY_URL", // replace later
+  timeout: 5000
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = token;
-  return config;
-});
+// TEMP: disable auth to avoid crashes
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = token;
+//   return config;
+// });
 
 export default API;
